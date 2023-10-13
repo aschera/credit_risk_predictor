004b3496ba31bcc42ed30384d4f718b0854eee46
Aschera <aschera@gmail.com>
2023-10-13 09:57:14 +0200

    code improvements on the web app.

--- app/app.py
+++ app/app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, request, jsonify
 import json
-import joblib
+import logging
 import numpy as np
-from helpers import extract_features  # Import the extract_features function
+from flask import Flask, render_template, request, jsonify
+import joblib
+from helpers import extract_features
 
 app = Flask(__name__)
 
 # Load the pre-trained model
 model = joblib.load('../model/logistic_regression_model.pkl')
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @app.route('/')
 def index():
@@ -15,24 +20,37 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the JSON data from the request
-    request_data = request.get_json()
+    try:
+        # Get the JSON data from the request
+        request_data = request.get_json()
 
-    print(request_data )
+        # Log the request data
+        logger.info('Received request data: %s', request_data)
 
-    # Extract the necessary features for prediction using the extract_features function
-    #  features = extract_features(request_data)
+        # Extract the necessary features for prediction using the extract_features function
+        features = extract_features(request_data)
 
-    # Convert features to a 2D array for model prediction
-    # features_array = np.array([features])
+        # Convert features to a 2D array for model prediction
+        features_array = np.array([features])
 
-    # Make prediction
-    # prediction = model.predict(features_array)
-    # predicted_class = int(prediction[0])
+        # Make prediction
+        prediction = model.predict(features_array)
+        predicted_class = int(prediction[0])
 
-    # Return prediction as JSON response
-    response_data = {'prediction': 'accepted'}
-    # return json.dumps(response_data), 200, {'Content-Type': 'application/json'}
+        # Define prediction result based on the model output
+        prediction_result = 'accepted' if predicted_class == 1 else 'rejected'
+
+        # Log the prediction result
+        logger.info('Prediction: %s', prediction_result)
+
+        # Return prediction as JSON response
+        response_data = {'prediction': prediction_result}
+        return jsonify(response_data), 200
+
+    except Exception as e:
+        # Log and return an error message in case of an exception
+        logger.error('Error predicting: %s', str(e))
+        return jsonify({'error': 'An error occurred during prediction'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/static/script.js
+++ app/static/script.js
@@ -1,15 +1,68 @@
+// Event Listeners
+document.addEventListener('DOMContentLoaded', function () {
+    // Materialize select elements
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+});
 
+document.getElementById('loanForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    handleFormSubmission();
+});
+
+// Handle Form Submission
+async function handleFormSubmission() {
+    const formData = gatherFormData();
+    try {
+        const predictionData = await fetchPredictionData(formData);
+        updatePrediction(predictionData);
+    } catch (error) {
+        console.error('Error predicting:', error);
+    }
+}
+
+// Gather Form Data
+function gatherFormData() {
+    // Gather all the form values
+    const formData = {};
+    const formElements = [
+        'age', 'income', 'race', 'sex', 'ethnicity', 'lenderCredits',
+        'debtToIncomeRatio', 'creditScore', 'loanAmount', 'interestRate',
+        'totalpointsandfees', 'loanterm', 'discountPoints', 'prepaymentPenaltyTerm',
+        'negativeAmortization', 'totalloancosts', 'loantype', 'loanpurpose',
+        'originationCharges', 'interestOnlyPayment', 'balloonPayment',
+        'otherNonamortizingFeatures'
+    ];
+
+    formElements.forEach(element => {
+        formData[element] = document.getElementById(element).value;
+    });
+
+    return formData;
+}
+
+// Fetch Prediction Data
+async function fetchPredictionData(formData) {
+    const response = await fetch('/predict', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams(formData)
+    });
+    return await response.json();
+}
+
+// Update Prediction
+function updatePrediction(data) {
+    const predictionElement = document.getElementById('prediction');
+    predictionElement.innerText = `Predicted class: ${data.prediction}`;
+}
+
+
+/* ----------------------Populate form with dummy data----------------------------   */
+// Populate dropdown with dummy data
 let dummyData;
-
-// Load dummy data from JSON
-fetch('./static/testdata.json')
-    .then(response => response.json())
-    .then(data => {
-        dummyData = data;
-        console.log('Dummy data loaded:', dummyData);
-        populateDropdown();
-    })
-    .catch(error => console.error('Error loading dummy data:', error));
 
 function populateDropdown() {
     const dropdown = document.getElementById('dummyDataDropdown');
@@ -23,62 +76,53 @@ function populateDropdown() {
     }
 
     // Initialize Materialize select elements
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems);
+    const elems = document.querySelectorAll('select');
+    M.FormSelect.init(elems);
 
     console.log('Dropdown populated with options:', dropdown.innerHTML);
 }
 
-
-
+// Fill form with dummy data based on the selected option
 function fillFormWithDummyDataAndPredict() {
     const selectedOption = document.getElementById('dummyDataDropdown').value;
     const data = dummyData[selectedOption];
 
     if (data) {
         // Fill the form fields with dummy data
-        document.getElementById('first_name').value = data.applicantData.first_name;
-        document.getElementById('last_name').value = data.applicantData.last_name;
-        document.getElementById('age').value = data.applicantData.age;
-        document.getElementById('race').value = data.applicantData.race;
-        document.getElementById('sex').value = data.applicantData.sex;
-        document.getElementById('ethnicity').value = data.applicantData.ethnicity;
-        document.getElementById('income').value = data.applicantData.income;
-        document.getElementById('debtToIncomeRatio').value = data.applicantData.debtToIncomeRatio;
-        document.getElementById('creditScore').value = data.applicantData.creditScore;
-        document.getElementById('lenderCredits').value = data.applicantData.lenderCredits;
+        const formFields = [
+            'first_name', 'last_name', 'age', 'race', 'sex', 'ethnicity', 'income',
+            'debtToIncomeRatio', 'creditScore', 'lenderCredits', 'applicantAge',
+            'applicantRace', 'applicantSex', 'applicantEthnicity', 'propertyvalue',
+            'lienstatus', 'manufacturedHomeLandPropertyInterest', 'multifamilyAffordableUnits',
+            'occupancyType', 'manufacturedHomeSecuredPropertyType', 'totalUnits', 'loanAmount',
+            'interestRate', 'totalpointsandfees', 'loanterm', 'discountPoints',
+            'prepaymentPenaltyTerm', 'negativeAmortization', 'totalloancosts', 'loantype',
+            'loanpurpose', 'originationCharges', 'interestOnlyPayment', 'balloonPayment',
+            'otherNonamortizingFeatures'
+        ];
 
-        document.getElementById('applicantAge').value = data.coApplicantData.applicantAge;
-        document.getElementById('applicantRace').value = data.coApplicantData.applicantRace;
-        document.getElementById('applicantSex').value = data.coApplicantData.applicantSex;
-        document.getElementById('applicantEthnicity').value = data.coApplicantData.applicantEthnicity;
-
-        document.getElementById('propertyvalue').value = data.propertyInfo.propertyValue;
-        document.getElementById('lienstatus').value = data.propertyInfo.lienStatus;
-        document.getElementById('manufacturedHomeLandPropertyInterest').value = data.propertyInfo.manufacturedHomeLandPropertyInterest;
-        document.getElementById('multifamilyAffordableUnits').value = data.propertyInfo.multifamilyAffordableUnits;
-        document.getElementById('occupancyType').value = data.propertyInfo.occupancyType;
-        document.getElementById('manufacturedHomeSecuredPropertyType').value = data.propertyInfo.manufacturedHomeSecuredPropertyType;
-        document.getElementById('totalUnits').value = data.propertyInfo.totalUnits;
-
-        document.getElementById('loanAmount').value = data.loanDetails.loanAmount;
-        document.getElementById('interestRate').value = data.loanDetails.interestRate;
-        document.getElementById('totalpointsandfees').value = data.loanDetails.totalpointsandfees;
-        document.getElementById('loanterm').value = data.loanDetails.loanterm;
-        document.getElementById('discountPoints').value = data.loanDetails.discountPoints;
-        document.getElementById('prepaymentPenaltyTerm').value = data.loanDetails.prepaymentPenaltyTerm;
-        document.getElementById('negativeAmortization').value = data.loanDetails.negativeAmortization;
-        document.getElementById('totalloancosts').value = data.loanDetails.totalloancosts;
-        document.getElementById('loantype').value = data.loanDetails.loantype;
-        document.getElementById('loanpurpose').value = data.loanDetails.loanpurpose;
-        document.getElementById('originationCharges').value = data.loanDetails.originationCharges;
-        document.getElementById('interestOnlyPayment').value = data.loanDetails.interestOnlyPayment;
-        document.getElementById('balloonPayment').value = data.loanDetails.balloonPayment;
-        document.getElementById('otherNonamortizingFeatures').value = data.loanDetails.otherNonamortizingFeatures;
+        formFields.forEach(field => {
+            if (data[field] !== undefined) {
+                document.getElementById(field).value = data[field];
+            }
+        });
     } else {
         console.error('No data found for the selected option.');
     }
 }
+
+// Load dummy data from JSON
+fetch('./static/testdata.json')
+    .then(response => response.json())
+    .then(data => {
+        dummyData = data;
+        console.log('Dummy data loaded:', dummyData);
+        populateDropdown();
+    })
+    .catch(error => console.error('Error loading dummy data:', error));
+
+
+/* ----------------------Predict loan acceptance from form data ---------------------------   */
 
 function predictLoan() {
     // Prepare the data to send to the Flask app
@@ -93,99 +137,28 @@ function predictLoan() {
     console.log('Data being sent:', jsonData);
 
     // Send the data to the Flask app for prediction
-    fetch('/predict', {
+    const predictionEndpoint = '/predict';
+
+    const requestOptions = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(jsonData)
-    })
-        .then(response => response.json())
+    };
+
+    fetch(predictionEndpoint, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
             console.log('Prediction:', data);
-            // Update your UI with the prediction
-            document.getElementById('prediction').innerText = `Predicted class: ${data.prediction}`;
+            // Update the UI with the prediction
+            const predictionElement = document.getElementById('prediction');
+            predictionElement.innerText = `Predicted class: ${data.prediction}`;
         })
         .catch(error => console.error('Error predicting:', error));
 }
-
-// Attach this function to the form submission
-document.getElementById('loanForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    predictLoan(); // Call the function to predict the loan
-});
-
-
-
-
-// Initialize Materialize select elements
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems);
-});
-
-document.querySelector('form').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    // Gather all the form values
-    const age = document.getElementById('age').value;
-    const income = document.getElementById('income').value;
-    const race = document.getElementById('race').value;
-    const sex = document.getElementById('sex').value;
-    const ethnicity = document.getElementById('ethnicity').value;
-    const lenderCredits = document.getElementById('lenderCredits').value;
-    const debtToIncomeRatio = document.getElementById('debtToIncomeRatio').value;
-    const creditScore = document.getElementById('creditScore').value;
-
-    const loanAmount = document.getElementById('loanAmount').value;
-    const interestRate = document.getElementById('interestRate').value;
-    const totalpointsandfees = document.getElementById('totalpointsandfees').value;
-    const loanterm = document.getElementById('loanterm').value;
-    const discountPoints = document.getElementById('discountPoints').value;
-    const prepaymentPenaltyTerm = document.getElementById('prepaymentPenaltyTerm').value;
-    const negativeAmortization = document.getElementById('negativeAmortization').value;
-    const totalloancosts = document.getElementById('totalloancosts').value;
-    const loantype = document.getElementById('loantype').value;
-    const loanpurpose = document.getElementById('loanpurpose').value;
-    const originationCharges = document.getElementById('originationCharges').value;
-    const interestOnlyPayment = document.getElementById('interestOnlyPayment').value;
-    const balloonPayment = document.getElementById('balloonPayment').value;
-    const otherNonamortizingFeatures = document.getElementById('otherNonamortizingFeatures').value;
-
-    fetch('/predict', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams({
-            'age': age,
-            'income': income,
-            'race': race,
-            'sex': sex,
-            'ethnicity': ethnicity,
-            'lenderCredits': lenderCredits,
-            'debtToIncomeRatio': debtToIncomeRatio,
-            'creditScore': creditScore,
-
-            'loanAmount': loanAmount,
-            'interestRate': interestRate,
-            'totalpointsandfees': totalpointsandfees,
-            'loanterm': loanterm,
-            'discountPoints': discountPoints,
-            'prepaymentPenaltyTerm': prepaymentPenaltyTerm,
-            'negativeAmortization': negativeAmortization,
-            'totalloancosts': totalloancosts,
-            'loantype': loantype,
-            'loanpurpose': loanpurpose,
-            'originationCharges': originationCharges,
-            'interestOnlyPayment': interestOnlyPayment,
-            'balloonPayment': balloonPayment,
-            'otherNonamortizingFeatures': otherNonamortizingFeatures,
-        })
-    })
-        .then(response => response.json())
-        .then(data => {
-            const predictionElement = document.getElementById('prediction');
-            predictionElement.innerText = `Predicted class: ${data.prediction}`;
-        });
-});
