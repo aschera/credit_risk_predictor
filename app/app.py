13c2cb07fe657d64a6c395c6412d22193318f8e6
Aschera <aschera@gmail.com>
2023-10-25 11:41:42 +0200

    updates to the web app.

--- app/app.py
+++ app/app.py
@@ -3,12 +3,20 @@ import logging
 import numpy as np
 from flask import Flask, render_template, request, jsonify
 import joblib
-from helpers import extract_features
+from flask import request
+
 
 app = Flask(__name__)
 
 # Load the pre-trained model
 model = joblib.load('../model/logistic_regression_model.pkl')
+
+# Define a dictionary to store the test data
+test_data = {}
+
+# Load test data from JSON and populate the test_data dictionary
+with open('./static/testdata.json', 'r') as json_file:
+    test_data = json.load(json_file)
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -18,39 +26,33 @@ logger = logging.getLogger(__name__)
 def index():
     return render_template('index.html')
 
+import numpy as np
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        # Get the JSON data from the request
+    if request.method == 'POST':
+        # Access the JSON data sent from the client
         request_data = request.get_json()
 
-        # Log the request data
-        logger.info('Received request data: %s', request_data)
+        # Create a list of values from the dictionary and convert them to a 2D array
+        input_data = [list(request_data.values())]
+        logger.info(input_data )
 
-        # Extract the necessary features for prediction using the extract_features function
-        features = extract_features(request_data)
 
-        # Convert features to a 2D array for model prediction
-        features_array = np.array([features])
+        # Make prediction using the pre-trained model
+         # prediction = model.predict(input_data)
+         # predicted_class = prediction
 
-        # Make prediction
-        prediction = model.predict(features_array)
-        predicted_class = int(prediction[0])
-
-        # Define prediction result based on the model output
-        prediction_result = 'accepted' if predicted_class == 1 else 'rejected'
+        # Define the "action taken" based on the model output
+         # action_taken = 'accepted' if predicted_class == 1 else 'rejected'
 
         # Log the prediction result
-        logger.info('Prediction: %s', prediction_result)
+         # logger.info('Action taken: %s', action_taken)
 
         # Return prediction as JSON response
-        response_data = {'prediction': prediction_result}
-        return jsonify(response_data), 200
-
-    except Exception as e:
-        # Log and return an error message in case of an exception
-        logger.error('Error predicting: %s', str(e))
-        return jsonify({'error': 'An error occurred during prediction'}), 500
+         # response_data = {'action_taken': action_taken}
+        return jsonify(input_data), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- app/static/script.js
+++ app/static/script.js
@@ -1,14 +1,4 @@
-// Event Listeners
-document.addEventListener('DOMContentLoaded', function () {
-    // Materialize select elements
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems);
-});
 
-document.getElementById('loanForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    handleFormSubmission();
-});
 
 // Handle Form Submission
 async function handleFormSubmission() {
@@ -20,8 +10,23 @@ async function handleFormSubmission() {
         console.error('Error predicting:', error);
     }
 }
+// Event Listeners
+document.addEventListener('DOMContentLoaded', function () {
+    // Materialize select elements
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
 
-// Gather Form Data
+    // Add an event listener to the form submit button
+    document.getElementById('loanForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        predictLoan();
+    });
+});
+
+/* ----------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------- */
+
+// Function to gather form data
 function gatherFormData() {
     // Gather all the form values
     const formData = {};
@@ -31,15 +36,92 @@ function gatherFormData() {
         'totalpointsandfees', 'loanterm', 'discountPoints', 'prepaymentPenaltyTerm',
         'negativeAmortization', 'totalloancosts', 'loantype', 'loanpurpose',
         'originationCharges', 'interestOnlyPayment', 'balloonPayment',
-        'otherNonamortizingFeatures'
+        'otherNonamortizingFeatures',
+        'CoapplicantAge', 'CoapplicantRace', 'CoapplicantSex', 'CoapplicantEthnicity',
+        'propertyValue', 'lienStatus', 'manufacturedHomeLandPropertyInterest',
+        'multifamilyAffordableUnits', 'occupancyType', 'manufacturedHomeSecuredPropertyType',
+        'totalUnits','race2','CoapplicantRace2'
     ];
 
+    const dataMappings = {
+        'age': 'applicant_age',
+        'sex': 'applicant_sex',
+        'race': 'applicant_race_1',
+        'ethnicity': 'applicant_ethnicity_1',
+        'loantype': 'loan_type',
+        'income': 'income',
+        'lenderCredits': 'lenderCredits',
+        'debtToIncomeRatio': 'debt_to_income_ratio',
+        'creditScoreType': 'applicant_credit_score_type',
+        'loanAmount': 'loanAmount',
+        'interestRate': 'interestRate',
+        'totalpointsandfees': 'totalpointsandfees',
+        'loanterm': 'loanterm',
+        'discountPoints': 'discountPoints',
+        'prepaymentPenaltyTerm': 'prepaymentPenaltyTerm',
+        'negativeAmortization': 'negativeAmortization',
+        'totalloancosts': 'totalloancosts',
+        'loantype': 'loan_type',
+        'loanpurpose': 'loanpurpose',
+        'originationCharges': 'originationCharges',
+        'interestOnlyPayment': 'interestOnlyPayment',
+        'balloonPayment': 'balloonPayment',
+        'otherNonamortizingFeatures': 'otherNonamortizingFeatures',
+        'CoapplicantAge': 'co_applicant_age',
+        'CoapplicantRace': 'co_applicant_race_1',
+        'CoapplicantSex': 'co_applicant_sex',
+        'CoapplicantEthnicity': 'co_applicant_ethnicity_1',
+        'propertyValue': 'propertyValue',
+        'lienStatus': 'lien_status',
+        'manufacturedHomeLandPropertyInterest': 'manufactured_home_land_property_interest',
+        'multifamilyAffordableUnits': 'multifamily_affordable_units',
+        'occupancyType': 'occupancy_type',
+        'manufacturedHomeSecuredPropertyType': 'manufactured_home_secured_property_type',
+        'totalUnits': 'total_units',
+        'race2' :'applicant_race_2',
+        'CoapplicantRace2':'co_applicant_race_2',
+    };
+
+    const intDtypes = [
+        'loan_type', 'lien_status', 'loan_amount', 'loan_term', 'negative_amortization',
+        'interest_only_payment', 'balloon_payment', 'other_nonamortizing_features',
+        'occupancy_type', 'manufactured_home_secured_property_type',
+        'manufactured_home_land_property_interest', 'total_units',
+        'debt_to_income_ratio', 'applicant_age', 'co_applicant_age', 'aus_1', 'applicant_credit_score_type'
+    ];
+
+    const floatDtypes = [
+        'census_tract', 'interest_rate', 'property_value', 'income',
+        'applicant_race_1', 'applicant_race_2', 'co_applicant_race_1',
+        'co_applicant_race_2', 'applicant_ethnicity_1', 'co_applicant_ethnicity_1'
+    ];
+    
+
     formElements.forEach(element => {
-        formData[element] = document.getElementById(element).value;
+        const inputElement = document.getElementById(element);
+        if (inputElement) {
+            const mappedName = dataMappings[element] || element; // Use the mapped name or the original if no mapping
+            let value = inputElement.value;
+
+            // Convert to the appropriate data type
+            if (intDtypes.includes(mappedName)) {
+                value = parseInt(value);
+            } else if (floatDtypes.includes(mappedName)) {
+                value = parseFloat(value);
+            }
+
+            formData[mappedName] = value;
+        }
     });
+
+    console.log(formData);
 
     return formData;
 }
+
+/* ----------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------- */
+
 
 // Fetch Prediction Data
 async function fetchPredictionData(formData) {
@@ -87,7 +169,7 @@ function fillFormWithDummyData() {
     const selectedOption = document.getElementById('dummyDataDropdown').value;
     const data = dummyData[selectedOption];
 
-    console.log('Fill with dummy data', data);
+    //console.log('Fill with dummy data', data);
 
     if (data) {
         // Iterate through the data object and its nested objects
@@ -112,49 +194,34 @@ fetch('./static/testdata.json')
     .then(response => response.json())
     .then(data => {
         dummyData = data;
-        console.log('Dummy data loaded:', dummyData);
+        //console.log('Dummy data loaded:', dummyData);
         populateDropdown();
     })
     .catch(error => console.error('Error loading dummy data:', error));
 
 
 /* ----------------------Predict loan acceptance from form data ---------------------------   */
+// Function to send form data for prediction
+async function predictLoan() {
+    // Define the data to be sent
+    const formData = gatherFormData(); // Assuming gatherFormData function is defined
 
-function predictLoan() {
-    // Prepare the data to send to the Flask app
-    const formData = new FormData(document.getElementById('loanForm'));
-    const jsonData = {};
-
-    formData.forEach((value, key) => {
-        jsonData[key] = value;
-    });
-
-    // Log the data being sent
-    console.log('Data being sent:', jsonData);
-
-    // Send the data to the Flask app for prediction
-    const predictionEndpoint = '/predict';
-
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(jsonData)
-    };
-
-    fetch(predictionEndpoint, requestOptions)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Prediction:', data);
-            // Update the UI with the prediction
+    // Make the AJAX request
+    $.ajax({
+        type: 'POST',
+        contentType: 'application/json',
+        url: '/predict', 
+        dataType: 'json',
+        data: JSON.stringify(formData), // Sending the formData
+        success: function (result) {
+            console.log('Received result:', result);
             const predictionElement = document.getElementById('prediction');
-            predictionElement.innerText = `Predicted class: ${data.prediction}`;
-        })
-        .catch(error => console.error('Error predicting:', error));
+            predictionElement.innerText = `Predicted class: ${result.prediction}`;
+        },
+        error: function (error) {
+            console.error('Error:', error);
+            const predictionElement = document.getElementById('prediction');
+            predictionElement.innerText = `error: ${error.message}`;
+        }
+    });
 }
