cfba417a85e8f3c61b1a39a7ee4097ea80056c2c
Aschera <aschera@gmail.com>
2023-11-01 10:25:34 +0100

    trying to make the model work. form adjustments. not yet ready.

--- app/app.py
+++ app/app.py
@@ -4,23 +4,19 @@ import numpy as np
 from flask import Flask, render_template, request, jsonify
 import joblib
 from flask import request
-
+import numpy as np
+import requests
+import json
+import logging
+import numpy as np
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
 
 app = Flask(__name__)
 
 # Load the pre-trained model
-model_xgboost = joblib.load('./xgboost_model.pkl') # excepts 33 features:
-
-expected_features = ['census_tract', 'loan_type', 'lien_status', 'open_end_line_of_credit', 
-                     'loan_amount', 'combined_loan_to_value_ratio', 'interest_rate', 'total_loan_costs', 
-                     'origination_charges', 'loan_term', 'negative_amortization', 'interest_only_payment', 
-                     'balloon_payment', 'other_nonamortizing_features', 'property_value', 'occupancy_type', 
-                     'manufactured_home_secured_property_type', 'manufactured_home_land_property_interest', 
-                     'total_units', 'income', 'debt_to_income_ratio', 'applicant_credit_score_type', 
-                     'co_applicant_credit_score_type', 'applicant_ethnicity_1', 'co_applicant_ethnicity_1', 
-                     'applicant_race_1', 'applicant_race_2', 'co_applicant_race_1', 
-                     'co_applicant_race_2', 'applicant_sex', 'co_applicant_sex', 'applicant_age', 
-                     'co_applicant_age']
+model_xgboost = joblib.load('../notebooks/Christinas/EDA_stepwise/xgboost_model.pkl') # excepts 33 features:
 
 # Define a dictionary to store the test data
 test_data = {}
@@ -37,38 +33,31 @@ logger = logging.getLogger(__name__)
 def index():
     return render_template('index.html')
 
-import numpy as np
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        # Access the JSON data sent from the client
-        request_data = request.get_json()
+        try:
+            # Access the JSON data sent from the client
+            request_data = request.get_json()
+            logger.info('request_data: %s', request_data)
 
-        # Create a list of values from the dictionary and convert them to a 2D array
-        input_data = [list(request_data.values())]
-        logger.info(input_data)
+            # Convert the request data to a pandas DataFrame
+            input_df = pd.DataFrame(request_data, index=[0])
 
-        # Create a dictionary that maps feature names to values
-        input_dict = dict(zip(expected_features, input_data[0]))
+            # Make prediction using the pre-trained model
+            prediction = model_xgboost.predict(input_df)
 
-        # Create prepared_data by accessing values in the expected order
-        prepared_data = [input_dict[feature] for feature in expected_features]
+            # Define the "action taken" based on the model output
+            action_taken = 'rejected' if prediction == 0 else 'accepted'
+            logger.info('Action taken: %s', action_taken)
 
-        # Make prediction using the pre-trained model
-        prediction = model_xgboost.predict([prepared_data])
-        predicted_class = prediction
-
-        # Define the "action taken" based on the model output
-        action_taken = 'accepted' if predicted_class == 1 else 'rejected'
-
-        # Log the prediction result
-        logger.info('Action taken: %s', action_taken)
-
-        # Return prediction as JSON response
-        response_data = {'action_taken': action_taken}
-        return jsonify(response_data), 200
-
+            # Return prediction as JSON response
+            response_data = {'action_taken': action_taken}
+            return jsonify(response_data), 200
+        except Exception as e:
+            logger.error('Prediction Error: %s', str(e))
+            return jsonify({'error': 'Prediction failed'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
