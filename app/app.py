ed88b85d569dd858f34febecfaec606dd7d1bc02
Aschera <aschera@gmail.com>
2023-11-02 11:46:17 +0100

    .

--- app/app.py
+++ app/app.py
@@ -1,72 +1,74 @@
 import json
 import logging
 import numpy as np
-from flask import Flask, render_template, request, jsonify
-import joblib
-from flask import request
-import numpy as np
-import requests
-import json
-import logging
-import numpy as np
-from flask import Flask, request, jsonify
-import joblib
 import pandas as pd
+from flask import Flask, render_template, request
+import joblib
 import pickle
+from flask import jsonify
+
+
 
 app = Flask(__name__)
 
 # Load the pre-trained model
-# model_xgboost = joblib.load('../notebooks/Christinas/EDA_stepwise/xgboost_model.pkl')
-model = pickle.load(open('../notebooks/Christinas/EDA_stepwise/xgboost_model.pkl', 'rb')) # Load the trained model
+model = pickle.load(open('../notebooks/Christinas/EDA_stepwise/xgboost_model.pkl', 'rb'))
 
 # Define the order of columns
-column_order = ['census_tract', 'action_taken', 'loan_type', 'lien_status', 
-                'open_end_line_of_credit', 'loan_amount', 'combined_loan_to_value_ratio', 
-                'interest_rate', 'total_loan_costs', 'origination_charges', 'loan_term', 
-                'negative_amortization', 'interest_only_payment', 'balloon_payment', 
-                'other_nonamortizing_features', 'property_value', 'occupancy_type', 
-                'manufactured_home_secured_property_type', 'manufactured_home_land_property_interest', 
-                'total_units', 'income', 'debt_to_income_ratio', 'applicant_credit_score_type', 
-                'co_applicant_credit_score_type', 'applicant_ethnicity_1', 'co_applicant_ethnicity_1', 
-                'applicant_race_1', 'applicant_race_2', 'co_applicant_race_1', 'co_applicant_race_2', 
-                'applicant_sex', 'co_applicant_sex', 'applicant_age', 'co_applicant_age']
-
+column_order = [
+    'census_tract', 'action_taken', 'loan_type', 'lien_status',
+    'open_end_line_of_credit', 'loan_amount', 'combined_loan_to_value_ratio',
+    'interest_rate', 'total_loan_costs', 'origination_charges', 'loan_term',
+    'negative_amortization', 'interest_only_payment', 'balloon_payment',
+    'other_nonamortizing_features', 'property_value', 'occupancy_type',
+    'manufactured_home_secured_property_type', 'manufactured_home_land_property_interest',
+    'total_units', 'income', 'debt_to_income_ratio', 'applicant_credit_score_type',
+    'co_applicant_credit_score_type', 'applicant_ethnicity_1', 'co_applicant_ethnicity_1',
+    'applicant_race_1', 'applicant_race_2', 'co_applicant_race_1', 'co_applicant_race_2',
+    'applicant_sex', 'co_applicant_sex', 'applicant_age', 'co_applicant_age'
+]
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+import numpy as np
+
 @app.route('/predict', methods=['POST'])
 def predict():
-
     init_features = []
-    final_features = []
 
-    if request.method == 'POST':
-        for column_name in column_order:
-            x = request.form.get(column_name, 'undefined')
-            if x.strip().lower() == 'undefined':
-                float_value = 0.0
-            try:
-                float_value = float(x)
-                init_features.append(float_value)
-            except ValueError:
-                float_value = 0.0
-                pass
+    
+    init_features = request.form.values()
+    
+    app.logger.info('Received data: %s', init_features)
 
-        logger.info('init_features: %s', init_features)
+    if init_features:
+        final_features = np.array(init_features)  # Create a NumPy array directly
+        final_features = final_features.reshape(1, -1)  # Reshape to match expected input shape
+        app.logger.info('final_features: %s', final_features)
+        
 
-        if init_features:
-            final_features = [np.array(init_features)]
-            prediction = model.predict(final_features)
-            
-            logger.info('prediction: %s', prediction)
+        prediction = model.predict(final_features)
+        prediction = prediction.tolist()  # Convert the ndarray to a list
+        app.logger.info('Prediction: %s', prediction)
+        return jsonify({"prediction": prediction})
+    else:
+        app.logger.info('Invalid input values')
+        return jsonify({"error": "Invalid input values"})
 
-            return render_template('index.html', prediction_text='Prediction: {}'.format(prediction))
-        else:
-            # Handle the case where no valid numeric values were found
-            return render_template('index.html', prediction_text='Invalid input values')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+# INFO:app:Received data: [2.0, 3.0, 2.0, 3.0]
+# INFO:app:final_features: [array([2., 3., 2., 3.])]
--- test/app.py
+++ test/app.py
@@ -1,9 +1,15 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+import logging
+
 
 app = Flask(__name__) # Initialize the flask App
 model = pickle.load(open('model.pkl', 'rb')) # Load the trained model
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 @app.route('/') # Homepage
 def home():
@@ -16,6 +22,11 @@ def predict():
     init_features = [float(x) for x in request.form.values()]
     final_features = [np.array(init_features)]
     prediction = model.predict(final_features) # Make a prediction
+
+    app.logger.info('Received data: %s', init_features)
+    app.logger.info('final_features: %s', final_features)
+        
+        
 
     return render_template('index.html', prediction_text='Predicted Species: {}'.format(prediction)) # Render the predicted result
 
