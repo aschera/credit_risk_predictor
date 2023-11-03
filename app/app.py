a3399356b55171bc28dc435631472d8bce95c078
Aschera <aschera@gmail.com>
2023-11-03 09:30:25 +0100

    finished :)

--- app/app.py
+++ app/app.py
@@ -1,22 +1,18 @@
-import json
 import logging
-import numpy as np
-import pandas as pd
 from flask import Flask, render_template, request
-import joblib
 import pickle
 from flask import jsonify
-
-
+from xgboost import XGBClassifier
+import numpy as np
 
 app = Flask(__name__)
 
 # Load the pre-trained model
-model = pickle.load(open('../model/xgboost_model.pkl', 'rb'))
+model = pickle.load(open('../model/xgboost_model_not_scaled.pkl', 'rb'))
 
 # Define the order of columns
 column_order = [
-    'census_tract', 'action_taken', 'loan_type', 'lien_status',
+    'loan_type', 'lien_status',
     'open_end_line_of_credit', 'loan_amount', 'combined_loan_to_value_ratio',
     'interest_rate', 'total_loan_costs', 'origination_charges', 'loan_term',
     'negative_amortization', 'interest_only_payment', 'balloon_payment',
@@ -28,6 +24,7 @@ column_order = [
     'applicant_sex', 'co_applicant_sex', 'applicant_age', 'co_applicant_age'
 ]
 
+
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -36,34 +33,29 @@ logger = logging.getLogger(__name__)
 def index():
     return render_template('index.html')
 
-
-
-import numpy as np
-
 @app.route('/predict', methods=['POST'])
 def predict():
+    
+    # Initialize an empty list for input features
     init_features = []
 
+    # Populate the list dynamically based on the order of columns
+    for column_name in column_order:
+        feature_value = request.form.get(column_name)
+        if feature_value is not None:
+            init_features.append(float(feature_value))
+
+    # Ensure that the list has the same number of values as the column_order
+    if len(init_features) != len(column_order):
+        return jsonify({"error": f"Input data must contain {len(column_order)} float values"})
+
+    # Perform the prediction
+    prediction = model.predict([init_features])
     
-    init_features = request.form.values()
-    
-    app.logger.info('Received data: %s', init_features)
+    # Convert the prediction to a regular Python list
+    prediction = prediction.tolist()
 
-    if init_features:
-        final_features = np.array(init_features)  # Create a NumPy array directly
-        final_features = final_features.reshape(1, -1)  # Reshape to match expected input shape
-        app.logger.info('final_features: %s', final_features)
-        
-
-        prediction = model.predict(final_features)
-        prediction = prediction.tolist()  # Convert the ndarray to a list
-        app.logger.info('Prediction: %s', prediction)
-        return jsonify({"prediction": prediction})
-    else:
-        app.logger.info('Invalid input values')
-        return jsonify({"error": "Invalid input values"})
-
-
+    return jsonify({"prediction": prediction})
 
 if __name__ == '__main__':
     app.run(debug=True)
