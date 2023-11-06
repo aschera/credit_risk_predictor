4ed3497acb86e87e5866cfb18975e56a7d4a5002
Chefmark <markus.v.ekman@gmail.com>
2023-11-06 09:01:21 +0100

    update

--- app/.ipynb_checkpoints/app-checkpoint.py
+++ app/.ipynb_checkpoints/app-checkpoint.py
@@ -1,11 +1,29 @@
+import json
+import logging
+import numpy as np
 from flask import Flask, render_template, request, jsonify
 import joblib
+from flask import request
 import numpy as np
+import requests
+import json
+import logging
+import numpy as np
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
 
 app = Flask(__name__)
 
 # Load the pre-trained model
-model = joblib.load('logistic_regression_model.pkl')
+model_xgboost = joblib.load('../notebooks/Christinas/EDA_stepwise/xgboost_model.pkl')
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Define the expected feature names in the correct order
+expected_feature_names = ['census_tract', 'loan_type', 'lien_status', 'open_end_line_of_credit', 'loan_amount', 'combined_loan_to_value_ratio', 'interest_rate', 'total_loan_costs', 'origination_charges', 'loan_term', 'negative_amortization', 'interest_only_payment', 'balloon_payment', 'other_nonamortizing_features', 'property_value', 'occupancy_type', 'manufactured_home_secured_property_type', 'manufactured_home_land_property_interest', 'total_units', 'income', 'debt_to_income_ratio', 'applicant_credit_score_type', 'co_applicant_credit_score_type', 'applicant_ethnicity_1', 'co_applicant_ethnicity_1', 'applicant_race_1', 'applicant_race_2', 'co_applicant_race_1', 'co_applicant_race_2', 'applicant_sex', 'co_applicant_sex', 'applicant_age', 'co_applicant_age']
 
 @app.route('/')
 def index():
@@ -13,46 +31,32 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the JSON data from the request
-    request_data = request.get_json()
+    if request.method == 'POST':
 
-    # Extract the necessary features for prediction
-    features = [
-        request_data['dummyData1']['applicantData']['age'],
-        request_data['dummyData1']['applicantData']['income'],
-        request_data['dummyData1']['applicantData']['race'],
-        request_data['dummyData1']['applicantData']['sex'],
-        request_data['dummyData1']['applicantData']['ethnicity'],
-        request_data['dummyData1']['applicantData']['lenderCredits'],
-        request_data['dummyData1']['applicantData']['debtToIncomeRatio'],
-        request_data['dummyData1']['applicantData']['creditScore'],
+            # Access the JSON data sent from the client
+            request_data = request.get_json()
 
-        request_data['dummyData1']['loanDetails']['loanAmount'],
-        request_data['dummyData1']['loanDetails']['interestRate'],
-        request_data['dummyData1']['loanDetails']['totalpointsandfees'],
-        request_data['dummyData1']['loanDetails']['loanterm'],
-        request_data['dummyData1']['loanDetails']['discountPoints'],
-        request_data['dummyData1']['loanDetails']['prepaymentPenaltyTerm'],
-        request_data['dummyData1']['loanDetails']['negativeAmortization'],
-        request_data['dummyData1']['loanDetails']['totalloancosts'],
-        request_data['dummyData1']['loanDetails']['loantype'],
-        request_data['dummyData1']['loanDetails']['loanpurpose'],
-        request_data['dummyData1']['loanDetails']['originationCharges'],
-        request_data['dummyData1']['loanDetails']['interestOnlyPayment'],
-        request_data['dummyData1']['loanDetails']['balloonPayment'],
-        request_data['dummyData1']['loanDetails']['otherNonamortizingFeatures']
-    ]
+            # Create a dictionary with feature names and values
+            input_data_dict = {}
 
+            for feature in expected_feature_names:
+                input_data_dict[feature] = float(request_data.get(feature, 0))
 
-    # Convert features to a 2D array for model prediction
-    features_array = np.array([features])
+            # Create a DataFrame from the dictionary
+            input_data_df = pd.DataFrame(input_data_dict, index=[0])
+            logger.info('input_data_df: %s', input_data_df)
 
-    # Make prediction
-    prediction = model.predict(features_array)
-    predicted_class = prediction[0]
+            # Make prediction using the pre-trained model
+            prediction = model_xgboost.predict(input_data_df)
+            logger.info('prediction: %s', prediction)
 
-    # Return prediction as JSON response
-    return jsonify({'prediction': predicted_class})
+            # Define the "action taken" based on the model output
+            action_taken = 'accepted' if prediction == 1 else 'rejected'
+
+            # Return prediction as JSON response
+            response_data = {'action_taken': action_taken}
+
+            return jsonify(response_data), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/app.py
+++ app/app.py
@@ -1,14 +1,32 @@
-import json
 import logging
+from flask import Flask, render_template, request
+import pickle
+from flask import jsonify
+from xgboost import XGBClassifier
 import numpy as np
-from flask import Flask, render_template, request, jsonify
 import joblib
-from helpers import extract_features
-
+import os
 app = Flask(__name__)
 
 # Load the pre-trained model
-model = joblib.load('../model/logistic_regression_model.pkl')
+model = pickle.load(open('../model/xgboost_model_not_scaled.pkl', 'rb'))
+
+# Load the explainer from the file
+explainer = joblib.load('../model/explainer.pkl')
+
+# Define the order of columns
+column_order = [
+    'loan_type', 'lien_status',
+    'open_end_line_of_credit', 'loan_amount', 'combined_loan_to_value_ratio',
+    'interest_rate', 'total_loan_costs', 'origination_charges', 'loan_term',
+    'negative_amortization', 'interest_only_payment', 'balloon_payment',
+    'other_nonamortizing_features', 'property_value', 'occupancy_type',
+    'manufactured_home_secured_property_type', 'manufactured_home_land_property_interest',
+    'total_units', 'income', 'debt_to_income_ratio', 'applicant_credit_score_type',
+    'co_applicant_credit_score_type', 'applicant_ethnicity_1', 'co_applicant_ethnicity_1',
+    'applicant_race_1', 'applicant_race_2', 'co_applicant_race_1', 'co_applicant_race_2',
+    'applicant_sex', 'co_applicant_sex', 'applicant_age', 'co_applicant_age'
+]
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -20,37 +38,34 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        # Get the JSON data from the request
-        request_data = request.get_json()
+    
+    # Initialize an empty list for input features
+    init_features = []
 
-        # Log the request data
-        logger.info('Received request data: %s', request_data)
+    # Populate the list dynamically based on the order of columns
+    for column_name in column_order:
+        feature_value = request.form.get(column_name)
+        if feature_value is not None:
+            init_features.append(float(feature_value))
 
-        # Extract the necessary features for prediction using the extract_features function
-        features = extract_features(request_data)
+    # Ensure that the list has the same number of values as the column_order
+    if len(init_features) != len(column_order):
+        return jsonify({"error": f"Input data must contain {len(column_order)} float values"})
 
-        # Convert features to a 2D array for model prediction
-        features_array = np.array([features])
+    # Perform the prediction
+    prediction = model.predict([init_features])
+    
+    # Convert the prediction to a regular Python list
+    prediction = prediction.tolist()
 
-        # Make prediction
-        prediction = model.predict(features_array)
-        predicted_class = int(prediction[0])
+    # Set the prediction_text variable
+    prediction_text = f"Prediction: {prediction[0]}"
 
-        # Define prediction result based on the model output
-        prediction_result = 'accepted' if predicted_class == 1 else 'rejected'
+    # Use the explainer to generate explanations
+    explanation = explainer.shap_values(init_features)
 
-        # Log the prediction result
-        logger.info('Prediction: %s', prediction_result)
-
-        # Return prediction as JSON response
-        response_data = {'prediction': prediction_result}
-        return jsonify(response_data), 200
-
-    except Exception as e:
-        # Log and return an error message in case of an exception
-        logger.error('Error predicting: %s', str(e))
-        return jsonify({'error': 'An error occurred during prediction'}), 500
+    # Return a JSON response with both prediction_text and explanation
+    return jsonify({"prediction_text": prediction_text, "explanation": explanation.tolist()})
 
 if __name__ == '__main__':
     app.run(debug=True)
